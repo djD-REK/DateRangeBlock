a63a5e604c54f0f51eb03c7bbca81b27d61bbbcf
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-18 13:02:01 -0400

    Update Block.js

--- src/Block.js
+++ src/Block.js
@@ -2,10 +2,52 @@ import React from "react"
 import { css, StyleSheet } from "aphrodite/no-important"
 import { getStyles } from "./getStyles"
 import { defaultConfig } from "./configs"
+import { moment } from "moment-timezone"
 
 const Block = (props) => {
+  const {
+    borderColor,
+    dateRange,
+    startTime,
+    endTime,
+    timeZone,
+    text,
+    utils,
+  } = props
+  const [startDate, endDate] = dateRange
+  const currentTime = new Date()
+  // Correct current time based on UTC
+  currentTime.setHours()
+
   const classes = StyleSheet.create(getStyles(props))
-  return <h1 className={css(classes.example)}>{props.text}</h1>
+
+  return (
+    <React.Fragment>
+      {(currentTime < 3 || utils.isRendering) && (
+        <div style={{ color: borderColor, fontStyle: "italics" }}>
+          The sales banner will be displayed from
+          {startTime}
+          {timeZone >= 0 && "+"}
+          {timeZone}UTC on {startDate} to
+          {endTime}
+          {timeZone >= 0 && "+"}
+          {timeZone}UTC on {endDate}
+        </div>
+      )}
+      {utils.isRendering && (
+        <div style={{ color: borderColor, fontStyle: "italics" }}>
+          The sales banner will be displayed from
+          {startTime}
+          {timeZone >= 0 && "+"}
+          {timeZone}UTC on {startDate} to
+          {endTime}
+          {timeZone >= 0 && "+"}
+          {timeZone}UTC on {endDate}
+        </div>
+      )}
+      <div className={css(classes.banner)}>{text}</div>
+    </React.Fragment>
+  )
 }
 
 Block.defaultProps = defaultConfig
