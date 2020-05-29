c254b9d3329475c5882d5f4afabc7977391a41c9
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-05-29 15:06:58 -0400

    Removed section headers

--- src/configs.js
+++ src/configs.js
@@ -1,15 +1,9 @@
 import { ElementPropTypes } from "@volusion/element-proptypes"
 
 export const configSchema = {
-  section1: {
-    type: ElementPropTypes.sectionHeader,
-  },
   dateRange: {
     label: "Sale Dates (Start and End Dates)",
     type: ElementPropTypes.dateRange,
-  },
-  section2: {
-    type: ElementPropTypes.sectionHeader,
   },
   text: {
     label: "Text Content",
@@ -17,13 +11,12 @@ export const configSchema = {
   },
 }
 
-// Set up default configuration for dateRange picker (Sales Dates)
+// Set up default configuration for the dateRange picker
 const today = new Date() // start date
 const nextWeek = new Date() // end date
 nextWeek.setDate(nextWeek.getDate() + 7)
+
 export const defaultConfig = {
-  section1: "Sale Dates",
   dateRange: [today, nextWeek],
-  section2: "Banner Text",
   text: "50% Off All Sale Items",
 }
